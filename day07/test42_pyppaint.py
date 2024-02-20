fd2b0a84efba5191c560fd5fe9d3004d6c37b9be
y7pWuXAq <mylullaby_@naver.com>
2024-02-20 17:27:55 +0900

    #240202 실행파일 만들기

--- day07/test42_pyppaint.py
+++ day07/test42_pyppaint.py
@@ -16,8 +16,10 @@ class WinApp(QWidget) :
         self.initSignal()
 
     def initUI(self) : # 화면 초기화
-        uic.loadUi('./day07/pyPaint.ui', self)
-        self.setWindowIcon(QIcon('./images/iot.png'))
+        # uic.loadUi('./day07/pyPaint.ui', self)
+        uic.loadUi('pyPaint.ui', self) # 실행파일 생성시는 경로에 상대경로가 없어져야함
+        # self.setWindowIcon(QIcon('./images/iot.png'))
+        self.setWindowIcon(QIcon('iot.png'))
         self.setWindowTitle('Py 그림판')
        
         # 캔버스 초기화
